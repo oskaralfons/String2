58b89621a86acaaa5724fccf84577ed1c2c7261f
art <art@niklar.com>
2015-03-14 23:25:48 +0100

    added equals and hashCode methods

--- src/main/java/com/niklar/string2/String2.java
+++ src/main/java/com/niklar/string2/String2.java
@@ -22,4 +22,21 @@ public class String2 implements Comparable<String2>{
     public int compareTo(final String2 o) {
         return this.toString().compareTo(o.toString());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        String2 string2 = (String2) o;
+
+        if (!Arrays.equals(wordArray, string2.wordArray)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return wordArray != null ? Arrays.hashCode(wordArray) : 0;
+    }
 }
